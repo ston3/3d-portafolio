4addbeff4ce5c5c1d10c0c719059235769b2ab45
Rainer Flores <rainer.flores64@gmail.com>
2024-01-11 22:39:13 -0400

    added Home component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,13 +12,14 @@ export const Navbar = () => {
        <nav className="flex text-lg gap-7 font-medium">
         <NavLink 
           to="/about"
-          className={({isActive}) => isActive? 'text-blue-500' : 'text-black'}
+          className={({isActive}) => isActive? 
+          "text-blue-500" : "text-black"}
         >
           About
         </NavLink>
         <NavLink 
           to="/projects"
-          className={({isActive}) => isActive? 'text-blue-500' : 'text-black'}
+          className={({isActive}) => isActive? "text-blue-500" : "text-black"}
         >
           Projects
         </NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
+import { Loader } from "../components/Loader"
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        BOMM!
+      </div>
+    <Canvas 
+      className="w-full h-screen bg-transparent"
+      camera={{near:0.1, far:1000}}
+    >
+    <Suspense fallback={<Loader/>}>
+      <directionalLight/>
+      <ambientLight/>
+      <pointLight/>
+      <spotLight/>
+      <hemisphereLight/>
+
+    </Suspense>
+
+    </Canvas>
+    
+    </section>
   )
 }
