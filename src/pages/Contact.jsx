e644fb4846f6ee1d0d0469be64255a999077845b
Rainer Flores <rainer.flores64@gmail.com>
2024-01-17 20:26:31 -0400

    added fox model to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,26 @@
-import { useRef, useState } from "react"
+import { Suspense, useRef, useState } from "react"
 import emailjs from "@emailjs/browser"
+import { Canvas } from "@react-three/fiber"
+import { Fox } from "../models/Fox"
+import { Loader } from "../components/Loader"
+import { useAlert } from "../hooks/useAlert"
+import { Alert } from "../components/Alert"
 
 export const Contact = () => {
   const formRef = useRef(null)
   const [form, setForm] = useState({name:'', email:'',message:''})
   const [isLoading, setIsLoading] = useState(false)
+  const [currentAnimation, setCurrentAnimation] = useState('idle')
+  const {alert, showAlert, hideAlert} = useAlert()
 
   const handleChange = (e) => {
     setForm({...form, [e.target.name] : e.target.value})
   }
 
-  const handleSubmit  = (e) => {
+  const handleSubmit  = (e) => {  
     e.preventDefault()
     setIsLoading(true)
+    setCurrentAnimation('hit')
 
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
@@ -21,32 +29,37 @@ export const Contact = () => {
         from_name: form.name,
         to_name: "Rainer",
         from_email: form.email,
-        to_email:"rainer.flores64@gmail.com",
+        to_email: "rain@0f88d.onmicrosoft.com",
         message: form.message
       },
       import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEYS
     ).then(() => {
       setIsLoading(false)
-      //TODO: Show success message
-      //TODO: Hide an alert
-      setForm({name:'', email:'', message:''})
+      showAlert({show: true, text: 'Message sent Successfully', type:'success'})
+
+      setTimeout(()=>{
+        hideAlert()
+        setCurrentAnimation('idle')
+        setForm({name:'', email:'', message:''})
+      },[3000])
     }).catch(error => {
       setIsLoading(false)
+      setCurrentAnimation('idle')
       console.log(error)
-      //TODO: show error message
+      showAlert({show: true, text: "I didn't receive your message", type:'danger'})
+
     })
 
   }
-  const handleFocus = () => {
 
-  }
-  const handleBlur = () => {
-
-  }
+  const handleFocus = () => setCurrentAnimation('walk')
+  const handleBlur = () => setCurrentAnimation('idle')
 
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert}/>}
+      <Alert {...alert}/>
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
         <form 
@@ -105,6 +118,24 @@ export const Contact = () => {
             {isLoading ? 'Sending' : 'Send Message'}
           </button>
         </form>
+      </div>
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{
+            position: [0, 0, 5]
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0,0,1]}/>
+          <ambientLight intensity={0.5}/>
+          <Suspense fallback={<Loader />} >
+            <Fox 
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   )
